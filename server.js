fbbb48b91909cff8a55418c66b6e660121b8fe72
jatinexist-pixel <jatinexist@gmail.com>
2026-05-10 13:15:16 +0530

    server.js

--- server.js
+++ server.js
@@ -29,4 +29,6 @@ app.post('/chat', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`);});
+app.listen(PORT, () => {
+    console.log(Server running on port ${PORT});
+});
